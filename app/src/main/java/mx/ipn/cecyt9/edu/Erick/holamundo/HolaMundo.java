afaf4c271830c8dd008dcbd9d55036670b77f2f4
ErickFlores6IM7 <eddycool37@live.com>
2019-01-25 08:53:56 -0600

    Initial commit

---
+++ app/src/main/java/mx/ipn/cecyt9/edu/Erick/holamundo/HolaMundo.java
@@ -0,0 +1,13 @@
+package mx.ipn.cecyt9.edu.Erick.holamundo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HolaMundo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hola_mundo);
+    }
+}
